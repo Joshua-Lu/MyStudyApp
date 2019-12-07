1e545dcdb4580b8a25a6e37b76ea8a91cb0a9f73
Joshua <2644792332@qq.com>
2019-12-07 22:39:02 +0800

    DragView实现方式三：使用scrollBy或scrollTo

--- app/src/main/java/com/joshua/myapplication/view/DragView.java
+++ app/src/main/java/com/joshua/myapplication/view/DragView.java
@@ -8,7 +8,7 @@ import android.view.View;
 
 /**
  * 该View实现自身跟随手指拖动效果
- *
+ * <p>
  * Created by Joshua on 2019/12/3 .
  */
 public class DragView extends View {
@@ -16,6 +16,11 @@ public class DragView extends View {
     private static final String TAG = "DragView";
     private int lastX;
     private int lastY;
+    private int toX;
+    private int toY;
+    int x0;// DragView的初始屏幕x坐标
+    int y0;// DragView的初始屏幕y坐标
+    int[] location = new int[2];
 
     public DragView(Context context) {
         this(context, null);
@@ -28,7 +33,16 @@ public class DragView extends View {
 
     public DragView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        setClickable(true);
+        setClickable(true);// 不设这个onTouchEvent无法正常回调
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        getLocationOnScreen(location);
+        x0 = location[0];
+        y0 = location[1];
+        Log.d(TAG, "onLayout: x0 = [" + x0 + "], y0 = [" + y0 + "]");
     }
 
     @Override
@@ -37,24 +51,44 @@ public class DragView extends View {
         int x = (int) event.getX();
         int y = (int) event.getY();
         Log.d(TAG, "onTouchEvent: x = [" + x + "], y = [" + y + "]");
+        int rawX = (int) event.getRawX();
+        int rawY = (int) event.getRawY();
+        Log.d(TAG, "onTouchEvent: rawX = [" + rawX + "], rawY = [" + rawY + "]");
         int action = event.getAction();
         switch (action) {
             case MotionEvent.ACTION_DOWN:
                 lastX = x;
                 lastY = y;
+                toX = -(rawX - x - x0);
+                toY = -(rawY - y - y0);
+                Log.d(TAG, "onTouchEvent ACTION_DOWN: toX = [" + toX + "], toY = [" + toY + "]");
                 break;
             case MotionEvent.ACTION_MOVE:
                 int offsetX = x - lastX;
                 int offsetY = y - lastY;
-                Log.d(TAG, "onTouchEvent: offsetX = [" + offsetX + "], offsetY = [" + offsetY + "]");
+                Log.d(TAG, "onTouchEvent ACTION_MOVE: offsetX = [" + offsetX + "], offsetY = [" + offsetY + "]");
+
                 // 方式一：使用layout
 //                layout(getLeft() + offsetX,
 //                        getTop() + offsetY,
 //                        getRight() + offsetX,
 //                        getBottom() + offsetY);
+
                 // 方式二：使用offsetLeftAndRight和offsetTopAndBottom
+                // 推荐使用该方式，简单易懂
                 offsetLeftAndRight(offsetX);
                 offsetTopAndBottom(offsetY);
+
+                // 方式三：使用scrollBy或scrollTo
+                // 注意：scrollBy或scrollTo移动的时View的视图，相当于反方向移动里面的内容
+                // 因此该方式移动的时父View里的所有内容
+                // 不推荐该方式
+//                ((View) getParent()).scrollBy(-offsetX, -offsetY);
+
+//                toX -= offsetX;
+//                toY -= offsetY;
+//                Log.d(TAG, "onTouchEvent ACTION_MOVE: toX = [" + toX + "], toY = [" + toY + "]");
+//                ((View) getParent()).scrollTo(toX, toY);
                 break;
         }
         return super.onTouchEvent(event);
