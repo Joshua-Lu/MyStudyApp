864623b3e57630f4a073aadeac4c92ad157df6b6
Joshua <47053655+Joshua-Lu@users.noreply.github.com>
2019-12-24 22:03:14 +0800

    修改Button点击事件实现方式

--- app/src/main/java/com/joshua/myapplication/MainActivity.java
+++ app/src/main/java/com/joshua/myapplication/MainActivity.java
@@ -3,52 +3,31 @@ package com.joshua.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_test_drag;
-    Button btn_test_viewDragHelper;
-    Button btn_test_draw;
-    Button btn_test_xmldraw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        btn_test_drag = findViewById(R.id.btn_test_drag);
-        btn_test_drag.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startTestActivity(TestDragActivity.class);
-            }
-        });
+    public void btnTestDragClick(View view) {
+        startTestActivity(TestDragActivity.class);
+    }
 
-        btn_test_viewDragHelper = findViewById(R.id.btn_test_viewDragHelper);
-        btn_test_viewDragHelper.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startTestActivity(TestViewDragHelperActivity.class);
-            }
-        });
+    public void btnTestViewDragHelperClick(View view) {
+        startTestActivity(TestViewDragHelperActivity.class);
+    }
 
-        btn_test_draw = findViewById(R.id.btn_test_draw);
-        btn_test_draw.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startTestActivity(TestDrawActivity.class);
-            }
-        });
+    public void btnTestDrawClick(View view) {
+        startTestActivity(TestDrawActivity.class);
+    }
 
-        btn_test_xmldraw = findViewById(R.id.btn_test_xmldraw);
-        btn_test_xmldraw.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startTestActivity(TestXmlDrawActivity.class);
-            }
-        });
+    public void btnTestXmldrawClick(View view) {
+        startTestActivity(TestXmlDrawActivity.class);
     }
 
     private void startTestActivity(Class<?> cls) {
