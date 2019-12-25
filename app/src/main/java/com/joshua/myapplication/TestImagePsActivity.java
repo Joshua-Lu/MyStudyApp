ba8d22125c4282bdff7434c2e74f68aab4aabadb
Joshua <47053655+Joshua-Lu@users.noreply.github.com>
2019-12-25 23:24:21 +0800

    新增TestImagePsActivity，测试图片处理相关方法

---
+++ app/src/main/java/com/joshua/myapplication/TestImagePsActivity.java
@@ -0,0 +1,129 @@
+package com.joshua.myapplication;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+
+import com.joshua.myapplication.utils.ImageHelper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TestImagePsActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener, Spinner.OnItemSelectedListener {
+
+    private static int MAX_VALUE = 255;
+    private static int MID_VALUE = 127;
+    private ImageView mImageView;
+    private SeekBar mSeekbarhue, mSeekbarSaturation, mSeekbarLum;
+    private float mHue, mStauration, mLum;
+    private Bitmap bitmap;
+
+    private float[] mColorMatrix = new float[20];
+    Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_image_ps);
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.myicon);
+        mImageView = (ImageView) findViewById(R.id.imageview);
+        mSeekbarhue = (SeekBar) findViewById(R.id.seekbarHue);
+        mSeekbarSaturation = (SeekBar) findViewById(R.id.seekbarSaturation);
+        mSeekbarLum = (SeekBar) findViewById(R.id.seekbarLum);
+        mSeekbarhue.setOnSeekBarChangeListener(this);
+        mSeekbarSaturation.setOnSeekBarChangeListener(this);
+        mSeekbarLum.setOnSeekBarChangeListener(this);
+        mSeekbarhue.setMax(MAX_VALUE);
+        mSeekbarSaturation.setMax(MAX_VALUE);
+        mSeekbarLum.setMax(MAX_VALUE);
+        mSeekbarhue.setProgress(MID_VALUE);
+        mSeekbarSaturation.setProgress(MID_VALUE);
+        mSeekbarLum.setProgress(MID_VALUE);
+        mImageView.setImageBitmap(bitmap);
+        spinner = (Spinner) findViewById(R.id.spinner_colorMatrixs);
+        spinner.setOnItemSelectedListener(this);
+        mColorMatrix = ImageHelper.initMatrix;
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar,
+                                  int progress, boolean fromUser) {
+        switch (seekBar.getId()) {
+            case R.id.seekbarHue:
+                mHue = (progress - MID_VALUE) * 1.0F / MID_VALUE * 180;
+                break;
+            case R.id.seekbarSaturation:
+                mStauration = progress * 1.0F / MID_VALUE;
+                break;
+            case R.id.seekbarLum:
+                mLum = progress * 1.0F / MID_VALUE;
+                break;
+        }
+        mImageView.setImageBitmap(ImageHelper.handleImageEffect(
+                bitmap, mHue, mStauration, mLum));
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+    }
+
+    // 将矩阵值设置到图像
+    private void setImageMatrix(float[] matrix) {
+        Bitmap bmp = Bitmap.createBitmap(
+                bitmap.getWidth(),
+                bitmap.getHeight(),
+                Bitmap.Config.ARGB_8888);
+        ColorMatrix colorMatrix = new ColorMatrix();
+        colorMatrix.set(matrix);
+
+        Canvas canvas = new Canvas(bmp);
+        Paint paint = new Paint();
+        paint.setColorFilter(new ColorMatrixColorFilter(colorMatrix));
+        canvas.drawBitmap(bitmap, 0, 0, paint);
+        mImageView.setImageBitmap(bmp);
+    }
+
+    // 作用矩阵效果
+    public void btnChange(View view) {
+        setImageMatrix(mColorMatrix);
+    }
+
+    // 重置矩阵效果
+    public void btnReset(View view) {
+        setImageMatrix(ImageHelper.initMatrix);
+    }
+
+    public void handleImageNegative(View view) {
+        mImageView.setImageBitmap(ImageHelper.handleImageNegative(bitmap));
+    }
+
+    public void handleImagePixelsOldPhoto(View view) {
+        mImageView.setImageBitmap(ImageHelper.handleImagePixelsOldPhoto(bitmap));
+    }
+
+    public void handleImagePixelsRelief(View view) {
+        mImageView.setImageBitmap(ImageHelper.handleImagePixelsRelief(bitmap));
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        mColorMatrix = ImageHelper.getColorMatrix(position);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
---
+++ app/src/main/java/com/joshua/myapplication/utils/ImageHelper.java
@@ -0,0 +1,207 @@
+package com.joshua.myapplication.utils;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+
+public class ImageHelper {
+    // 初始矩阵，不会对原有颜色值进行任何变化
+    public static float[] initMatrix = new float[]{
+            1, 0, 0, 0, 0,
+            0, 1, 0, 0, 0,
+            0, 0, 1, 0, 0,
+            0, 0, 0, 1, 0
+    };
+    // 底片效果
+    public static float[] negativeMatrix = new float[]{
+            -1, 0, 0, 0, 255,
+            0, -1, 0, 0, 255,
+            0, 0, -1, 0, 255,
+            0, 0, 0, 1, 0
+    };
+    // 老照片效果
+    public static float[] oldPhotoMatrix = new float[]{
+            0.393f, 0.769f, 0.189f, 0, 0,
+            0.349f, 0.686f, 0.168f, 0, 0,
+            0.272f, 0.534f, 0.131f, 0, 0,
+            0, 0, 0, 1, 0
+    };
+    // 浮雕效果
+    public static float[] reliefMatrix = new float[]{
+            1, 0, 0, 0, 0,
+            0, 1, 0, 0, 0,
+            0, 0, 1, 0, 0,
+            0, 0, 0, 1, 0
+    };
+
+    public static Bitmap handleImageEffect(Bitmap bm,
+                                           float hue,
+                                           float saturation,
+                                           float lum) {
+        Bitmap bmp = Bitmap.createBitmap(
+                bm.getWidth(), bm.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bmp);
+        Paint paint = new Paint();
+
+        ColorMatrix hueMatrix = new ColorMatrix();
+        hueMatrix.setRotate(0, hue);
+        hueMatrix.setRotate(1, hue);
+        hueMatrix.setRotate(2, hue);
+
+        ColorMatrix saturationMatrix = new ColorMatrix();
+        saturationMatrix.setSaturation(saturation);
+
+        ColorMatrix lumMatrix = new ColorMatrix();
+        lumMatrix.setScale(lum, lum, lum, 1);
+
+        ColorMatrix imageMatrix = new ColorMatrix();
+        imageMatrix.postConcat(hueMatrix);
+        imageMatrix.postConcat(saturationMatrix);
+        imageMatrix.postConcat(lumMatrix);
+
+        paint.setColorFilter(new ColorMatrixColorFilter(imageMatrix));
+        canvas.drawBitmap(bm, 0, 0, paint);
+        return bmp;
+    }
+
+    public static Bitmap handleImageNegative(Bitmap bm) {
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        int color;
+        int r, g, b, a;
+
+        Bitmap bmp = Bitmap.createBitmap(width, height
+                , Bitmap.Config.ARGB_8888);
+
+        int[] oldPx = new int[width * height];
+        int[] newPx = new int[width * height];
+        bm.getPixels(oldPx, 0, width, 0, 0, width, height);
+
+        for (int i = 0; i < width * height; i++) {
+            color = oldPx[i];
+            r = Color.red(color);
+            g = Color.green(color);
+            b = Color.blue(color);
+            a = Color.alpha(color);
+
+            r = 255 - r;
+            g = 255 - g;
+            b = 255 - b;
+
+            if (r > 255) {
+                r = 255;
+            } else if (r < 0) {
+                r = 0;
+            }
+            if (g > 255) {
+                g = 255;
+            } else if (g < 0) {
+                g = 0;
+            }
+            if (b > 255) {
+                b = 255;
+            } else if (b < 0) {
+                b = 0;
+            }
+            newPx[i] = Color.argb(a, r, g, b);
+        }
+        bmp.setPixels(newPx, 0, width, 0, 0, width, height);
+        return bmp;
+    }
+
+    public static Bitmap handleImagePixelsOldPhoto(Bitmap bm) {
+        Bitmap bmp = Bitmap.createBitmap(bm.getWidth(), bm.getHeight(),
+                Bitmap.Config.ARGB_8888);
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        int color = 0;
+        int r, g, b, a, r1, g1, b1;
+
+        int[] oldPx = new int[width * height];
+        int[] newPx = new int[width * height];
+
+        bm.getPixels(oldPx, 0, bm.getWidth(), 0, 0, width, height);
+        for (int i = 0; i < width * height; i++) {
+            color = oldPx[i];
+            a = Color.alpha(color);
+            r = Color.red(color);
+            g = Color.green(color);
+            b = Color.blue(color);
+
+            r1 = (int) (0.393 * r + 0.769 * g + 0.189 * b);
+            g1 = (int) (0.349 * r + 0.686 * g + 0.168 * b);
+            b1 = (int) (0.272 * r + 0.534 * g + 0.131 * b);
+
+            if (r1 > 255) {
+                r1 = 255;
+            }
+            if (g1 > 255) {
+                g1 = 255;
+            }
+            if (b1 > 255) {
+                b1 = 255;
+            }
+
+            newPx[i] = Color.argb(a, r1, g1, b1);
+        }
+        bmp.setPixels(newPx, 0, width, 0, 0, width, height);
+        return bmp;
+    }
+
+    public static Bitmap handleImagePixelsRelief(Bitmap bm) {
+        Bitmap bmp = Bitmap.createBitmap(bm.getWidth(), bm.getHeight(),
+                Bitmap.Config.ARGB_8888);
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        int color = 0, colorBefore = 0;
+        int a, r, g, b;
+        int r1, g1, b1;
+
+        int[] oldPx = new int[width * height];
+        int[] newPx = new int[width * height];
+
+        bm.getPixels(oldPx, 0, bm.getWidth(), 0, 0, width, height);
+        for (int i = 1; i < width * height; i++) {
+            colorBefore = oldPx[i - 1];
+            a = Color.alpha(colorBefore);
+            r = Color.red(colorBefore);
+            g = Color.green(colorBefore);
+            b = Color.blue(colorBefore);
+
+            color = oldPx[i];
+            r1 = Color.red(color);
+            g1 = Color.green(color);
+            b1 = Color.blue(color);
+
+            r = (r - r1 + 127);
+            g = (g - g1 + 127);
+            b = (b - b1 + 127);
+            if (r > 255) {
+                r = 255;
+            }
+            if (g > 255) {
+                g = 255;
+            }
+            if (b > 255) {
+                b = 255;
+            }
+            newPx[i] = Color.argb(a, r, g, b);
+        }
+        bmp.setPixels(newPx, 0, width, 0, 0, width, height);
+        return bmp;
+    }
+
+    public static float[] getColorMatrix(int position) {
+        switch (position) {
+            case 0:
+                return negativeMatrix;
+            case 1:
+                return oldPhotoMatrix;
+            default:
+                return initMatrix;
+        }
+    }
+}
