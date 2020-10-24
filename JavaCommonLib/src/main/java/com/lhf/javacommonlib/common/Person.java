c95915e89b31f31ac3532c850f4a353cfa3341cc
Joshua <47053655+Joshua-Lu@users.noreply.github.com>
2020-10-24 12:16:10 +0800

    add StreamTest

--- JavaCommonLib/src/main/java/com/lhf/javacommonlib/common/Person.java
+++ JavaCommonLib/src/main/java/com/lhf/javacommonlib/common/Person.java
@@ -13,6 +13,10 @@ public class Person implements Serializable {
     public Person() {
     }
 
+    public Person(String name) {
+        this.name = name;
+    }
+
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
---
+++ JavaCommonLib/src/main/java/com/lhf/javacommonlib/stream/StreamTest.java
@@ -0,0 +1,46 @@
+package com.lhf.javacommonlib.stream;
+
+import com.lhf.javacommonlib.common.Person;
+
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+/**
+ * 使用 Stream 对集合进行操作
+ * Created by Joshua on 2020/10/24 10:26
+ */
+class StreamTest {
+    public static void main(String[] args) {
+        testListStream();
+
+        testArrayStream();
+    }
+
+    private static void testArrayStream() {
+        // Stream.of 可以传可变参数或数组
+        Stream<String> stringStream = Stream.of("路飞", "索隆", "娜美");
+
+        String[] names = {"路飞", "索隆", "娜美"};
+        Stream<String> stringStream1 = Stream.of(names);
+    }
+
+    private static void testListStream() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("路飞");
+        list.add("索隆");
+        list.add("娜美");
+        list.add("乌索普");
+        list.add("山治");
+        list.add("乔巴");
+        list.add("罗宾");
+        list.add("弗兰奇");
+        list.add("布鲁克");
+        list.add("甚平");
+
+        list.stream()
+                .filter(s -> s.length() == 3) // 过滤
+                .map(Person::new) // 映射
+                .skip(1) // 跳过
+                .forEach(System.out::println);// 遍历
+    }
+}
