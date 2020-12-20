7b4d84e1551565b47924a51cbc6450acb8194e83
Joshua <47053655+Joshua-Lu@users.noreply.github.com>
2020-12-20 16:47:51 +0800

    add CommonUtils.getUserInput

---
+++ JavaCommonLib/src/main/java/com/lhf/javacommonlib/util/CommonUtils.java
@@ -0,0 +1,19 @@
+package com.lhf.javacommonlib.util;
+
+import java.util.Scanner;
+
+/**
+ * Created by Joshua on 2020/11/24 13:28
+ */
+public class CommonUtils {
+    public static void main(String[] args) {
+        String input = CommonUtils.getUserInput("请输入：");
+        System.out.println("CommonUtilTest.getUserInput: input = [" + input + "]");
+    }
+
+    public static String getUserInput(String prompt) {
+        System.out.println(prompt);
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+}
