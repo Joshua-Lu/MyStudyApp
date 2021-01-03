096c8b2562e2392ba76c7702ba042a1d82a5e632
Joshua <47053655+Joshua-Lu@users.noreply.github.com>
2021-01-03 20:01:56 +0800

    add observer pattern example WeatherStation

--- DesignPatternlib/src/main/java/com/lhf/designpatternlib/observer/example/WeatherData.java
+++ DesignPatternlib/src/main/java/com/lhf/designpatternlib/observer/example/WeatherData.java
@@ -4,6 +4,12 @@ import java.util.Observable;
 
 /**
  * 天气数据，可观察者，继承自java.util.Observable类
+ * <p>
+ * 使用Java内置Observable类，相比自定义接口Observable实现的优缺点：
+ * 优点：不用自己实现addObserver、deleteObserver、notifyObservers等方法，使用简便
+ * 缺点：1.java.util.Observable类是一个类，不是接口，使用必须继承该类，Java又是单继承，导致无法继承其他类
+ * 2.java.util.Observable有些关键方法，如setChanged是protected的，导致只能使用继承，无法使用组合，来使用Observable
+ * <p>
  * Created by Joshua on 2021/1/3 18:04
  */
 public class WeatherData extends Observable {
